f18fc11aef4473339748a97c8ae217bd72812cab
JuanVimon3 <jvillamonsalve@gmail.com>
2025-05-27 08:05:19 -0500

    add all

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// index.js
+import "./styles.css";
+import './jay-wennington-N_Y88TWmGwA-unsplash.jpg'; 
+import { greeting } from "./greeting.js";
+
+const image = document.createElement("img");
+image.src = jay-wennington-N_Y88TWmGwA-unsplash;
+
+document.body.appendChild(image);
+console.log(greeting);
