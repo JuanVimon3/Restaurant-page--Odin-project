8f123959b8b7f7b97fc466243ac771e3d20831f8
JuanVimon3 <jvillamonsalve@gmail.com>
2025-06-11 09:37:29 -0500

    add image and header buttons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // index.js
 import "./styles.css";
-import './jay-wennington-N_Y88TWmGwA-unsplash.jpg'; 
+import photo from './jay-wennington-N_Y88TWmGwA-unsplash.jpg'; 
 import { greeting } from "./greeting.js";
 
 const image = document.createElement("img");
-image.src = jay-wennington-N_Y88TWmGwA-unsplash;
+image.src = photo;
 
 document.body.appendChild(image);
 console.log(greeting);
